67c964def6fe51e4079bb53814e388ba6704e07d
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-08-10 23:13:28 +0200

    Add files via upload

---
+++ HomeworkWebApi/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomeworkWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+       public IActionResult GetAll()
+        {
+            var response = StaticDb.Users;
+            return Ok(response);
+        }
+
+        [HttpGet("GetOneUsers")]
+        public IActionResult GetOneUser()
+        {
+            
+            return Ok("Dejan");
+        }
+    }
+}
+
