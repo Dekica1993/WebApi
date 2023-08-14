00218cf9665ec74dcd7b32cf55f7cf02acc57d76
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-08-14 22:45:27 +0200

    Add files via upload

---
+++ Homeworks/SEDC.HomeworkClass03WebApi/HomeworkWebApi/HomeworkWebApi/StaticDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HomeworkWebApi
+{
+	public  static class StaticDb
+	{
+		public static List<string> Users = new List<string>
+		{
+			"Dejan",
+			"Damjan",
+			"Teo",
+			"Elena",
+			"Marija"
+
+		};
+	}
+}
+
